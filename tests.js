0ec31b602bd6f69d6d1c324cdc9e568ab14d3e10
rgowan <ranegowan@gmail.com>
2018-05-17 12:52:12 +0100

    robot can now be created with correct data

--- tests.js
+++ tests.js
@@ -10,5 +10,17 @@ describe('creating a new robot', () => {
     expect(robot.grid).to.eq('10 10');
     done();
   });
-  
+
+  it('should be given coordinates', done => {
+    const robot = new Robot('10 10', '5 5 N');
+    expect(robot.coordinates).to.eq('5 5 N');
+    done();
+  });
+
+  it('should be given instructions', done => {
+    const robot = new Robot('10 10', '5 5 N', 'FFLRF');
+    expect(robot.instructions).to.eq('FFLRF');
+    done();
+  });
+
 });
