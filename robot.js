c1071683a5ebbe6658a0b70bf3c72db25c57185a
rgowan <ranegowan@gmail.com>
2018-05-17 12:42:57 +0100

    setting up test script, first test passing

---
+++ robot.js
@@ -0,0 +1,9 @@
+class Robot {
+  constructor(grid, coordinates, instructions) {
+    this.grid = grid;
+    this.coordinates = coordinates;
+    this.instructions = instructions;
+  }
+}
+
+module.exports = Robot; 
